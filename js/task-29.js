8b2a4eb45311ad230df831e1b696d5a29ea300d7
hu <3278889655@qq.com>
2016-04-02 16:29:49 +0800

    IFE

---
+++ js/task-29.js
@@ -0,0 +1,41 @@
+var validate = document.getElementById("validate");
+var message = document.getElementById("message");
+var confirm = document.getElementById("confirm");
+var validateInput = function () {
+    var value = validate.value;
+    if (value.length === 0) {
+        showMessage(message, 0, "red");
+    } else {
+        value = value.replace(/^\s+|\s$/g, "").replace(/[\u0391-\uFFE5]/g, "--");
+        console.log(value);
+        if (value.length < 4 || value.length > 16) {
+            showMessage(message, 1, "red");
+        } else {
+            showMessage(message, 2, "green");
+        }
+    }
+};
+var showMessage = function (obj, result, textColor) {
+    if (result === 0) {
+        obj.innerHTML = "输入为空";
+        obj.style.color = textColor;
+    }
+    if (result === 1) {
+        obj.innerHTML = "长度不符合要求";
+        obj.style.color = textColor;
+    }
+    if (result === 2) {
+        obj.innerHTML = "输入格式正确";
+        obj.style.color = textColor;
+    }
+};
+var addEvent = function (element, event, listener) {
+    if (element.addEventListener) { //标准
+        element.addEventListener(event, listener, false);
+    } else if (element.attachEvent) { //低版本ie
+        element.attachEvent("on" + event, listener);
+    } else { //都不行的情况
+        element["on" + event] = listener;
+    }
+};
+addEvent(confirm, "click", validateInput);
