1a280859c51a80502c1f259a418efc396531d90a
hu <3278889655@qq.com>
2016-04-13 15:09:12 +0800

    IFE

--- js/task-37.js
+++ js/task-37.js
@@ -67,6 +67,35 @@
             cover.className = "pop-up";
             cover.appendChild(section);
             body.appendChild(cover);
+            /**
+             * 如果允许缩放
+             */
+            if (para.allowResize) {
+                var div = document.createElement("div");
+                div.className = "resizeL";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeT";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeR";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeB";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeLT";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeTR";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeBR";
+                section.appendChild(div);
+                div = document.createElement("div");
+                div.className = "resizeLB";
+                section.appendChild(div);
+            }
         },
         /**
          * 初始化
@@ -78,11 +107,11 @@
             this.cover = document.querySelector(".pop-up");
             this.wrap = this.cover.querySelector(".pop-wrap");
             //设置弹出层的宽和高
-            this.width = para.width;
-            this.height = para.height;
+            this.wrap.width = para.width;
+            this.wrap.height = para.height;
             this.wrap.style.width = para.width + "px";
             this.wrap.style.height = para.height + "px";
-            //设置边距的时候不能直接offsetWidth或offsetWidth，会导致结果为0
+            //设置边距的时候不能直接offsetWidth或offsetHeight，会导致结果为0，因为此时弹出层没有显示出来，offsetWidth或offsetHeight是计算值，结果为0
             this.wrap.style.marginLeft = -parseInt(this.getCss(this.wrap, "width")) / 2 + "px";
             this.wrap.style.marginTop = -parseInt(this.getCss(this.wrap, "height")) / 2 + "px";
             //给标题区添加内容
@@ -97,8 +126,10 @@
             this.status = false;
             //设置页面滚动标志
             this.allowPageWheel = para.allowPageWheel;
-            //设置页面拖拽标志
+            //设置弹出层拖拽标志
             this.allowDrag = para.allowDrag;
+            //设置弹出层缩放标志
+            this.allowResize = para.allowResize;
             //给确认和取消按钮添加回调事件
             var that = this,
                 confirm = this.cover.querySelector(".confirm"),//这里注意，如果每次点击按钮都新创建一个对象的话，那么这里的事件会重复绑定，导致alert多次
@@ -122,7 +153,75 @@
             if (this.allowDrag) {
                 this.drag();
             }
+            if (this.allowResize) {
+               this.popupResize();
+            }
             return this;
+        },
+        /**
+         * 弹出层缩放
+         */
+        popupResize: function () {
+            var oL = document.getElementsByClassName("resizeL")[0];
+            var oT = document.getElementsByClassName("resizeT")[0];
+            var oR = document.getElementsByClassName("resizeR")[0];
+            var oB = document.getElementsByClassName("resizeB")[0];
+            var oLT = document.getElementsByClassName("resizeLT")[0];
+            var oTR = document.getElementsByClassName("resizeTR")[0];
+            var oBR = document.getElementsByClassName("resizeBR")[0];
+            var oLB = document.getElementsByClassName("resizeLB")[0];
+            //四角
+            this.resize(this.wrap, oLT, true, true, false, false);
+            this.resize(this.wrap, oTR, false, true, false, false);
+            this.resize(this.wrap, oBR, false, false, false, false);
+            this.resize(this.wrap, oLB, true, false, false, false);
+            //四边
+            this.resize(this.wrap, oL, true, false, false, true);
+            this.resize(this.wrap, oT, false, true, true, false);
+            this.resize(this.wrap, oR, false, false, false, true);
+            this.resize(this.wrap, oB, false, false, true, false);
+        },
+        resize: function (oParent, handle, isLeft, isTop, lockX, lockY) {
+            var that = this;
+            this.addEvent(handle, "mousedown", function (event) {
+                event = event || window.event;
+                var disX = event.clientX-handle.offsetLeft;//初始X
+                var disY = event.clientY-handle.offsetTop;//初始Y
+                var iParentTop = oParent.offsetTop;//浮出层相对于父容器的上边距
+                var iParentLeft = oParent.offsetLeft;//浮出层相对于父容器的左边距
+                var iParentWidth = oParent.offsetWidth;//浮出层的宽度
+                var iParentHeight = oParent.offsetHeight;//浮出层的高度
+                document.onmousemove = function (event) {
+                    event = event || window.event;
+                    var iL = event.clientX - disX;
+                    var iT = event.clientY - disY;
+                    var maxW = document.documentElement.clientWidth - oParent.offsetLeft - 2;//最大宽度   document.documentElement.clientWidth:当前浏览器的宽度
+                    var maxH = document.documentElement.clientHeight - oParent.offsetTop - 2;//最大高度   document.documentElement.clientHeight 当前浏览器的高度
+                    var iW = isLeft ? iParentWidth - iL : handle.offsetWidth + iL; //判断是否是可以左右伸缩
+                    var iH = isTop ? iParentHeight - iT : handle.offsetHeight + iT;//判断是否可以上下伸缩
+                    var dragMinWidth = that.wrap.width;  //设置最小宽度
+                    var dragMinHeight = that.wrap.height;//设置最小高度
+
+                    isLeft && (oParent.style.left = iParentLeft + iL + that.wrap.width / 2 + "px");//因为定位是top:50% left:50% margin-left margin-right定位的 所以 left:还要在+上1半的宽度
+                    isTop && (oParent.style.top = iParentTop + iT + that.wrap.height / 2 + "px");
+
+                    iW < dragMinWidth && (iW = dragMinWidth); //判断最小宽度
+                    iW > maxW && (iW = maxW);
+                    lockX || (oParent.style.width = iW + "px");
+
+                    iH < dragMinHeight && (iH = dragMinHeight);//判断最小高度
+                    lockY || (oParent.style.height = iH + "px");
+                    iH > maxH && (iH = maxH);
+
+                    if((isLeft && iW == dragMinWidth) || (isTop && iH == dragMinHeight)) document.onmousemove = null;
+                    return false;
+                };
+                document.onmouseup = function () {
+                    document.onmousemove = null;
+                    document.onmouseup = null;
+                };
+                return false;
+            });
         },
         /**
          * 获取样式
@@ -141,32 +240,31 @@
          * 拖拽功能
          */
         drag: function () {
-            var that = this.wrap;
-            var self = this;
+            var that = this;
             this.header.style.cursor = "move";
             this.addEvent(this.header, "mousedown", function (event) {
+                event = event || window.event;
                 var disY,
                     disX;
-                event = event || window.event;
-                disX = event.clientX - parseInt(self.getCss(that, "left"));
-                disY = event.clientY - parseInt(self.getCss(that, "top"));
+                disX = event.clientX - that.wrap.offsetLeft;
+                disY = event.clientY - that.wrap.offsetTop;
                 document.onmousemove = function (event) {
                     event = event || window.event;
-                    var tempX = event.clientX - disX,
-                        tempY = event.clientY - disY;
+                    var tempX = event.clientX - disX + that.wrap.width / 2,
+                        tempY = event.clientY - disY + that.wrap.height / 2;
                     //拖拽时不能超过视窗边界
-                    if (tempX > document.documentElement.offsetWidth - that.offsetWidth / 2) {
-                        tempX = document.documentElement.offsetWidth - that.offsetWidth / 2;
-                    } else if (tempX < that.offsetWidth / 2) {
-                        tempX = that.offsetWidth / 2;
+                    if (tempX > document.documentElement.offsetWidth - that.wrap.offsetWidth + that.wrap.width / 2) {
+                        tempX = document.documentElement.offsetWidth - that.wrap.offsetWidth + that.wrap.width / 2;
+                    } else if (tempX < that.wrap.width / 2) {
+                        tempX = that.wrap.width / 2;
                     }
-                    if (tempY > document.documentElement.offsetHeight - that.offsetHeight / 2) {
-                        tempY = document.documentElement.offsetHeight - that.offsetHeight / 2;
-                    } else if (tempY < that.offsetHeight / 2) {
-                        tempY = that.offsetHeight / 2;
+                    if (tempY > document.documentElement.offsetHeight - that.wrap.offsetHeight + that.wrap.height / 2) {
+                        tempY = document.documentElement.offsetHeight - that.wrap.offsetHeight + that.wrap.height / 2;
+                    } else if (tempY < that.wrap.height / 2) {
+                        tempY = that.wrap.height / 2;
                     }
-                    that.style.left = tempX + "px";
-                    that.style.top = tempY + "px";
+                    that.wrap.style.left = tempX + "px";
+                    that.wrap.style.top = tempY + "px";
                 };
                 document.onmouseup = function () {
                     document.onmousemove = null;
