024c49bbda38290bc98d4fa5cecc9f0fff6bf118
hu <3278889655@qq.com>
2016-07-03 20:31:43 +0800

    test

---
+++ WebContent/cartjs/base.js
@@ -0,0 +1,37 @@
+function renderList(arr, parent) {
+    var li,
+        count = 0,
+        total = 0;
+    if (arr.length == 0) {
+        li = document.createElement("li");
+        li.className = "ccbg2";
+        li.innerHTML = '<div class="orderdish"><span class="name">菜篮子空空哒~~~</span></div>';
+        parent.appendChild(li);
+        return;
+    }
+    for (var i = 0; i < arr.length; i++) {
+        li = document.createElement("li");
+        li.className = "ccbg2";
+        li.innerHTML = '<div class="orderdish"><span class="name">' + arr[i].name + '</span><span class="price"> ' + arr[i].number + '份</span><span class="price"> 每份' + arr[i].price + '元</span></div>';
+        parent.appendChild(li);
+        count += arr[i].number;
+        total += arr[i].number * arr[i].price;
+    }
+    li = document.createElement("li");
+    li.className = "ccbg2";
+    li.innerHTML = '<div class="orderdish"><span class="price">共计' + count + '份</span><span class="price"> ￥' + total + '元</span></div>';
+    parent.appendChild(li);
+}
+function addEvent(element, type, handler) {
+    if (element.addEventListener) {
+        element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + type, handler);
+    } else {
+        element["on" + type] = handler;
+    }
+}
+addEvent(window, "load", function (e) {
+    var ullist = document.getElementById("ullist");
+    renderList(JSON.parse(localStorage.getItem("cart"))._products, ullist);
+});
---
+++ WebContent/foodjs/main.js
@@ -0,0 +1,119 @@
+function g(id) {
+    return document.getElementById(id);
+}
+function addProduct(productId, specId, name, price, categoryId, addnum) {
+    cart.addProduct(OAK.Shop.Product({
+        id: productId, 
+        specId: specId, 
+        number: addnum, 
+        price: price, 
+        name: name,
+        categoryId: categoryId
+    }));
+}
+function reduceProduct(productId, specId,num) {
+    var oldnum = cart.getProductNumber({
+        id: productId, 
+        specId: specId
+    });
+    if (oldnum !== null) {
+        if (oldnum - num > 0) {
+            cart.updateNumber(oldnum - num, {
+                id: productId, 
+                specId: specId
+            });
+        } else {
+            cart.deleteProduct({
+                id: productId, 
+                specId: specId
+            });
+        }
+    }
+}
+function showAll() {
+    var dts = document.getElementsByTagName("dt");
+    for (var i in dts) {
+        if (dts[i].innerText != null) {
+            dts[i].style.display = "";
+            var dd = dts[i].nextElementSibling;
+            while (dd != null && dd.tagName != 'DT') {
+                dd.style.display = "";
+                dd = dd.nextElementSibling
+            }
+        }
+    }
+    showMenu(false);
+}
+function showProducts(categoryName) {
+    showAll();
+    var dts = document.getElementsByTagName("dt");
+    for (var i in dts) {
+        if (dts[i].innerText != null && dts[i].innerText != categoryName) {
+            dts[i].style.display = "none";
+            var dd = dts[i].nextElementSibling;
+            while (dd != null && dd.tagName != 'DT' ) {
+                dd.style.display = "none";
+                dd = dd.nextElementSibling
+            }
+        }
+    }
+}
+function addEvent(element, type, handler) {
+    if (element.addEventListener) {
+        element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + type, handler);
+    } else {
+        element["on" + type] = handler;
+    }
+}
+function goToCart() {
+    location.href = "cart.html";
+    OAK.Util.setParam("cart", JSON.stringify(cart));
+}
+
+var cart = new OAK.Shop.Cart();
+cart.showProductNum = function (productId, specId, num) {
+    if (num > 0) {
+        g("num_" + productId + "_" + specId).className = "count";
+        //g("del_" + productId+"_"+specId).style.display = "";
+    } else {
+        g("num_" + productId + "_" + specId).className = "count_zero";
+        //g("del_"  + productId+"_"+specId).style.display = "none";
+    }
+    g("num_" + productId + "_" + specId).innerHTML = parseInt(num);
+}
+cart.showTotalNum = function () {
+    var quant = cart.getQuantity();
+    g("cartN").innerHTML = "" + quant.totalNumber + "份　￥" + quant.totalAmount.toFixed(2);
+    SetCookie("food1",quant.totalNumber);
+};
+cart.showCartInfo = function () {
+    var products = cart.getProductList();
+    for (var i in products) {
+        var product_id = products[i].id;
+        var spec_id = products[i].specId;
+        cart.showProductNum(product_id, spec_id, cart.getProductNumber({
+                id: product_id,
+                specId: spec_id
+            }) || 0);
+    }
+    cart.showTotalNum();
+};
+cart.onAfterAdd = function (obj, num, conditions) {
+    cart.showProductNum(conditions.id, conditions.specId, num);
+    cart.showTotalNum();
+    cart.saveToCache();
+};
+cart.onAfterUpdate = function (obj, num, conditions) {
+    cart.showProductNum(conditions.id, conditions.specId, num);
+    cart.showTotalNum();
+    cart.saveToCache();
+};
+cart.onAfterDelete = function (obj, conditions) {
+    cart.showProductNum(conditions.id, conditions.specId, 0);
+    cart.showTotalNum();
+    cart.saveToCache();
+};
+cart.getFromCache();
+cart.showCartInfo();
